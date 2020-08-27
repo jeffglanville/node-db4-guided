cce46604ee54869c30cf6dab715fbe0ea5f5406b
Jeff Glanville <jeff.glanville@outlook.com>
2020-08-26 19:50:10 -0500

    update other tables

--- data/migrations/20200826191937_initail.js
+++ data/migrations/20200826191937_initail.js
@@ -5,8 +5,35 @@ exports.up = async function(knex) {
       table.text("name").notNull()
       table.text("address").notNull().unique()
   })
+
+  await knex.schema.createTable("species", (table) => {
+    table.increments("id")
+    table.text("name").notNull().unique()
+  })
+
+  await knex.schema.createTable("animals", (table) => {
+    table.increments("id")
+    table.text("name").notNull()
+    table.integer("species_id")
+    .references("id")
+    .inTable("species")
+  })
+
+  await knex.schema.createTable("zoo_animals", (table) => {
+    table.integer("zoo_id").notNull()
+    .references("id")
+    .inTable("zoos")
+    table.integer("animals_id").notNull()
+    .references("id")
+    .inTable("animals")
+    table.date("from_date").notNull().defaultTo(knex.raw("current_timestamp"))
+    table.date("to_date")
+  })
 }
 
 exports.down = async function(knex) {
+  await knex.schema.dropTableIfExists("zoo_animals")
+  await knex.schema.dropTableIfExists("animals")
+  await knex.schema.dropTableIfExists("species")
   await knex.schema.dropTableIfExists("zoos")
 }
